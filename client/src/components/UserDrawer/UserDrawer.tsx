b4fde788c15974814128e8661357048560ec89b9
Artem Khalturin <goxalk@gmail.com>
2022-10-28 15:08:54 +0300

    add user drawer

---
+++ client/src/components/UserDrawer/UserDrawer.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import s from "./UserDrawer.module.scss";
+import logo from "../../assets/logo.svg";
+import userDefaultLogo from "../../assets/userDefaultLogo.svg";
+import help from "../../assets/help.svg";
+import cart from "../../assets/cart.svg";
+import account from "../../assets/account.svg";
+import exit from "../../assets/exit.svg";
+import closeBtn from "../../assets/closeBtn.svg";
+
+
+interface UserDrawerProps {
+    isUserDrawerOpened: boolean
+    onClickCloseUserDrawer: () => void
+}
+
+
+const UserDrawer: React.FC<UserDrawerProps> = ({isUserDrawerOpened, onClickCloseUserDrawer}) => {
+    return (
+        <div className={`${s.overlay} ${isUserDrawerOpened ? s.overlayOut : ""}`}>
+            <div className={s.drawer}>
+                <div className={s.top}>
+                    <img src={logo} alt="logo"/>
+                    <div className={s.blockUser}>
+                        <img src={userDefaultLogo} alt="userDefaultLogo"/>
+                        <div className={s.info}>
+                            <p className={s.name}>User</p>
+                            <p className={s.email}>useruser@mail.com</p>
+                        </div>
+                    </div>
+                    <div className={s.menu}>
+                        <ul>
+                            <li>
+                                <img src={cart} alt="cart"/>
+                                <p>Cart</p>
+                            </li>
+                            <li>
+                                <img src={account} alt="account"/>
+                                <p>Account</p>
+                            </li>
+                            <li>
+                                <img src={help} alt="help" width="41px"/>
+                                <p>Help</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={s.bottom}>
+                    <img src={exit} alt="help"/>
+                    <p>Exit</p>
+                </div>
+                <div className={s.closeBtn} onClick={onClickCloseUserDrawer}>
+                    <img src={closeBtn} alt="closeBtn"/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UserDrawer;
