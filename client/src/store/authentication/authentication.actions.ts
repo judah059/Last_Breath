ba9d7b11cee969908ff2da6c330611fe99dbc118
Artem Khalturin <goxalk@gmail.com>
2022-10-29 20:37:23 +0300

    update signUp form

--- client/src/store/authentication/authentication.actions.ts
+++ client/src/store/authentication/authentication.actions.ts
@@ -1,11 +1,19 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import {userAPI} from "../../utils/api";
-import {IReqUser, IResUser} from "../../utils/api/types";
+import {ICreateUser, IReqUser, IResUser} from "../../utils/api/types";
 
 
 export const login = createAsyncThunk<IResUser, IReqUser>(
     'user/login',
     async (userData, thunkAPI) => {
         return await userAPI.login(userData);
+    }
+)
+
+
+export const register = createAsyncThunk<ICreateUser, ICreateUser>(
+    'user/register',
+    async (userData, thunkAPI) => {
+        return await userAPI.register(userData);
     }
 )
--- client/src/utils/api/index.ts
+++ client/src/utils/api/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-import {ICinema, IReqUser, IResUser} from "./types";
+import {ICinema, ICreateUser, IReqUser, IResUser} from "./types";
+import {register} from "../../store/authentication/authentication.actions";
 
 let baseApi = axios.create({
     baseURL: 'https://6358280cc26aac906f3d1b80.mockapi.io/'
@@ -20,8 +21,9 @@ export let userAPI = {
     login(data: IReqUser) {
         return axios.post<IResUser>(`http://127.0.0.1:8000/api/token/`, data).then(res => res.data);
     },
+    register(data: ICreateUser){
+        return axios.post<ICreateUser>(`http://127.0.0.1:8000/api/registration/`, data).then(res => res.data);
+    },
 
-    getMe(){
-        return axios.get(`http://127.0.0.1:8000/api/myuser/`).then(res => res);
-    }
+
 }
