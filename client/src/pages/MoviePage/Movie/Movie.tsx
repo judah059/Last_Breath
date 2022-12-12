c75ed8908f3c5dbec9ae153e5ad86bb9cf90c6aa
Андрей Тарасенко <mikatohimemiya@gmail.com>
2022-12-12 17:54:35 +0200

    Updated:  - MoviePage bug's  - Movie bug's

--- client/src/pages/MoviePage/Movie/Movie.tsx
+++ client/src/pages/MoviePage/Movie/Movie.tsx
@@ -1,7 +1,9 @@
+// movie
+
 import React, {useEffect, useState} from 'react';
 import s from "./Movie.module.scss";
 import mov from "../../../assets/tempMovieImage.jpg";
-import {IMovieItem, ISessionByDate, ITestMovieItem} from "../../../utils/api/types";
+import {IMovieItem, ISession, ISessionByDate, ITestMovieItem} from "../../../utils/api/types";
 import {useNavigate, useParams} from "react-router-dom";
 import {API} from "../../../utils/api";
 import {setSessionById} from "../../../store/session/session.slice";
@@ -19,63 +21,83 @@ interface MovieProps {
 const Movie: React.FC<MovieProps> = (props) => {
     const dispatch = useAppDispatch();
     const navigate = useNavigate()
-   const [movie, setMovie] = useState<ITestMovieItem | undefined>(undefined);
-   const [noHalls, setNoHallse] = useState<Boolean>(false)
-   const [sessionByDateAndCinema, setSessionByDateAndCinema] = useState<ISessionByDate>()
-   const {cinema, isCinemaPage} = useAppSelector((state: RootState) => state.cinema);
+    const [movie, setMovie] = useState<ITestMovieItem | undefined>(undefined);
+    const [noHalls, setNoHallse] = useState<Boolean>(false)
+    const [sessionByDateAndCinema, setSessionByDateAndCinema] = useState<ISessionByDate>()
+    const {cinema, isCinemaPage} = useAppSelector((state: RootState) => state.cinema);
+    const [session, setSession] = useState<ISession[] | undefined>()
 
-   // const {id} = useParams() as { id: string };
+
+    // const {id} = useParams() as { id: string };
 
     const onClickTicketOpen = async (id: number) => {
         const data = await API.getSessionById(id)
         dispatch(setSessionById(data))
         navigate('/tickets-order')
     }
-   useEffect(() => {
-      fetchSessionBy()
-   }, [props.date])
+    useEffect(() => {
+        fetchSessionBy()
+        fetchSession()
+    }, [props.date])
 
-   const fetchSessionBy = async () => {
-      const movie: ITestMovieItem = await API.getCinemaMovie(props.movie_id);
-      setMovie(movie)
-      const sessionsByDate: ISessionByDate[] = await API.getSessionByDate({date: props.date, cinema: cinema?.id});
-      let sessionByDate: ISessionByDate = sessionsByDate[0];
-      console.log(sessionByDate)
-      let hallsLength = sessionByDate.halls.length
-      let counter = 0
-      for (let i = 0; i < sessionByDate.halls.length; i++) {
-         if (sessionByDate.halls[i].sessions.every(element => element === null)) {
-            counter++
-            if (hallsLength === counter) {
-               setNoHallse(true)
-               console.log("No halls true")
-               break;
-            } else continue;
-
-         }
-
-         for (let j = 0; j < sessionByDate.halls[i].sessions.length; j++) {
-            if (movie && sessionByDate.halls[i].sessions[j]?.movie != +movie?.id) {
-               sessionByDate.halls[i].sessions.splice(j, 1)
-               j--;
+    const fetchSessionBy = async () => {
+        const movie: ITestMovieItem = await API.getCinemaMovie(props.movie_id);
+        setMovie(movie)
+        const sessionsByDate: ISessionByDate[] = await API.getSessionByDate({date: props.date, cinema: cinema?.id});
+        let sessionByDate: ISessionByDate = sessionsByDate[0];
+        console.log(sessionByDate)
+        let hallsLength = sessionByDate.halls.length
+        let counter = 0
+        for (let i = 0; i < sessionByDate.halls.length; i++) {
+            for (let j = 0; j < sessionByDate.halls[i].sessions.length; j++) {
+                // @ts-ignore
+                if (movie && (sessionByDate.halls[i].sessions[j]?.movie != +movie?.id ||  sessionByDate.halls[i].sessions[j] == null ||  sessionByDate.halls[i].sessions[j] === "卐 1488")) {
+                    sessionByDate.halls[i].sessions.splice(j, 1)
+                    j--;
+                }
             }
-         }
-      }
-      if (hallsLength !== counter) setNoHallse(false);
+            // @ts-ignore
+            if (sessionByDate.halls[i]?.sessions.every(element => element === null) || Number(sessionByDate.halls[i]?.sessions.length) === 0 || sessionByDate.halls[i]?.sessions.every(element => element === "卐 1488")) {
 
-      for (let i = 0; i < sessionByDate.halls.length; i++) {
-         for (let j = 0; j < sessionByDate.halls[i].sessions.length; j++) {
-            if ((sessionByDate.halls[i].sessions[j] && sessionByDate.halls[i].sessions[j + 1]) && (Number(sessionByDate.halls[i].sessions[j].start_time.substring(0, 2)) > Number(sessionByDate.halls[i].sessions[j + 1].start_time.substring(0, 2)))) {
-               let temp = sessionByDate.halls[i].sessions[j];
-               sessionByDate.halls[i].sessions[j] = sessionByDate.halls[i].sessions[j + 1];
-               sessionByDate.halls[i].sessions[j + 1] = temp;
+                counter++
+                if (hallsLength === counter) {
+                    setNoHallse(true)
+                    console.log("No halls true")
+                    break;
+                } else continue;
+
             }
-         }
-      }
 
-      await setSessionByDateAndCinema(sessionByDate)
-      console.log(sessionByDate)
-   }
+
+        }
+        if (hallsLength !== counter) setNoHallse(false);
+
+        for (let i = 0; i < sessionByDate.halls.length; i++) {
+            for (let j = 0; j < sessionByDate.halls[i].sessions.length; j++) {
+                if ((sessionByDate.halls[i].sessions[j] && sessionByDate.halls[i].sessions[j + 1]) && (Number(sessionByDate.halls[i].sessions[j]?.start_time?.substring(0, 2)) > Number(sessionByDate.halls[i].sessions[j + 1]?.start_time?.substring(0, 2)))) {
+                    let temp = sessionByDate.halls[i].sessions[j];
+                    sessionByDate.halls[i].sessions[j] = sessionByDate.halls[i].sessions[j + 1];
+                    sessionByDate.halls[i].sessions[j + 1] = temp;
+                }
+            }
+        }
+
+        await setSessionByDateAndCinema(sessionByDate)
+        console.log(sessionByDate)
+    }
+    const fetchSession = async () => {
+        try {
+            const sessions: ISession[] = await API.getSession();
+            let sessionsMovie: ISession[] = sessions.filter(x => x.movie.toString() === movie?.id.toString())
+
+            setSession(sessionsMovie)
+
+            console.log(session)
+
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
     return (
         <div className={s.item}>
@@ -83,23 +105,27 @@ const Movie: React.FC<MovieProps> = (props) => {
                 <img src={props.poster} alt="" width={200}/>
                 <p>{props.name}</p>
             </div>
-            <ul className={s.timetable}>
-                {sessionByDateAndCinema?.halls.map((x, index) => <div key={index}>
-                    <p style={{margin: "0 0 8px 4px", textAlign: "start"}}>Hall #{x?.id}</p>
-                    <div style={{display: "flex", flexWrap: "wrap"}}>
-                        {x.sessions.map((s, index) => <li key={index} style={{margin: "4px 4px"}}
-                            onClick={() => onClickTicketOpen(x.sessions[index].id)}>
-                            {s.start_time.substring(s?.start_time?.length - 3, 0)}
-                        </li>)}
-                    </div>
-                </div>)}
-                {/*<li className={s.active}>11:00</li>*/}
-                {/*<li>12:00</li>*/}
-                {/*<li>13:00</li>*/}
-                {/*<li>14:00</li>*/}
-                {/*<li>15:00</li>*/}
-                {/*<li>16:00</li>*/}
-            </ul>
+            {
+                noHalls ?
+                    <ul className={s.timetable}>
+                        <div  style={{textAlign: "center",  background: '#231D2D', borderRadius: '9px', padding: '15px'}}>
+                            No sessions
+                        </div>
+                    </ul>
+                    :
+                    <ul className={s.timetable}>
+                        {sessionByDateAndCinema?.halls.map((x, index) => <div key={index}>
+                            <p style={{margin: "0 0 8px 4px", textAlign: "start"}}>Hall #{x?.id}</p>
+                            <div style={{display: "flex", flexWrap: "wrap"}}>
+                                {x.sessions.map((s, index) => <li key={index} style={{margin: "4px 4px"}}
+                                                                  onClick={() => onClickTicketOpen(x?.sessions[index].id)}>
+                                    {s?.start_time?.substring(s?.start_time?.length - 3, 0)}
+                                </li>)}
+                            </div>
+                        </div>)}
+                    </ul>
+            }
+
         </div>
     )
 };
